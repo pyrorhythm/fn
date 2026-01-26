dd8e196060558aadb74bf05334c7579f6440082d
pyro <me@pyrorhythm.dev>
2026-01-26 22:25:02 +0300

    feat(result): add ToPtr

--- result_functor.go
+++ result_functor.go
@@ -15,6 +15,21 @@ func To[T, U any](r Result[T], tf ...func(T) U) Result[U] {
 	return okv(tf[0](r.Val()))
 }
 
+// ToPtr is similar to [Morph], but specializes on error propagation.
+// If [Result.Exc] == true, propagates error without calling (and needing) the function.
+// Else, returns either a tf-call [Result], or a [ErrNoTFInTo] error.
+func ToPtr[T, U any](r Result[T], tf ...func(T) *U) Result[U] {
+	if r.Exc() {
+		return err[U](r.e)
+	}
+
+	if len(tf) == 0 {
+		return notf[U]()
+	}
+
+	return okp(tf[0](r.Val()))
+}
+
 // Morph casts [Result][T] to [Result][U] using func(T) [Result][U]. If [Result.Exc] == true, propagates error without calling the function.
 func Morph[T, U any](r Result[T], f func(T) Result[U]) Result[U] {
 	if r.OK() {
